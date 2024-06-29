0a7f1790f26b1c9e5f4e066e8464b0221433519c
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-06-29 20:52:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var pop= prompt("Enter Your Email");
+var password= prompt("Enter your Password");
+
+if(pop === "Kashan@gmail.com"){
+    alert("you are Kashan")
+}
+else if(password === "Kashan"){
+    alert("you are Admin")
+}
+else{
+    alert("You are user")
+}
